8eef68de5dee007ff9fda87ac778964f61f037dc
markthree <1801982702@qq.com>
2023-06-17 19:07:00 +0800

    feat(fn): add createOnce

--- src/fn.ts
+++ src/fn.ts
@@ -1,5 +1,15 @@
-// TODO {createOnce}
+import { AnyFunction } from "./type.ts";
 
 export function invoke<T>(fn: () => T): T {
   return fn();
+}
+
+export function createOnce<T extends AnyFunction>(fn: T) {
+  let resolved = false;
+  return function once(this: ThisType<T>, ...rest: Parameters<T>) {
+    if (!resolved) {
+      return fn.call(this, rest);
+    }
+    resolved = true;
+  };
 }
